848d0fbaa6845ec3d18b6b1d1f1254147e62c595
mariel-07 <ramosgomezdulce086@gmail.com>
2026-04-12 20:43:39 -0500

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const letras = "01ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const letras = "アァイィウヴエカキクケコサシスセソ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const fontSize = 16;
 const columnas = canvas.width / fontSize;
 
@@ -22,11 +22,11 @@ function dibujar() {
     ctx.font = fontSize + "px monospace";
 
     for (let i = 0; i < gotas.length; i++) {
-        const texto = letras[Math.floor(Math.random() * letras.length)];
+        const texto = letras.charAt(Math.floor(Math.random() * letras.length));
 
         ctx.fillText(texto, i * fontSize, gotas[i] * fontSize);
 
-        if (gotas[i] * fontSize > canvas.height) {
+        if (gotas[i] * fontSize > canvas.height && Math.random() > 0.975) {
             gotas[i] = 0;
         }
 
@@ -35,3 +35,8 @@ function dibujar() {
 }
 
 setInterval(dibujar, 35);
+
+window.addEventListener("resize", () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+});
